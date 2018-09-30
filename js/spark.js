de74f6334a016b32b1007da33e1706b9011d9297
lewatoto <lewatoto@localhost.localdomain>
2018-09-30 01:19:21 -0600

    Nueva version del sitio 31-09-2018

---
+++ js/spark.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+  // Check for click events on the navbar burger icon
+  $(".navbar-burger").click(function() {
+
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+
+  });
+
+  $(".cont").click(function() {
+
+    $(".modal").toggleClass("is-active");
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+
+  });
+
+  $(".modal-close").click(function() {
+
+    $(".modal").toggleClass("is-active");
+
+  });
+
+});
